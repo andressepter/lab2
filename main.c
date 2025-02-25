75750401cb3a8a3bd63acd76963ee37748eb01d2
Andres Septer <andres.septer@gmail.com>
2025-02-25 18:21:45 +0200

    bump-dump

--- main.c
+++ main.c
@@ -6,19 +6,38 @@
 #include "common/clock.h"
 #include "inc/msp432p401r.h"
 
-void DebugDump(uint8_t bump) {
-    // write this as part of Lab 2
+#define DEBUG_SIZE 256
+volatile uint8_t bumpDataArray[DEBUG_SIZE];
+volatile uint8_t lineDataArray[DEBUG_SIZE];
+volatile uint8_t debugIndex = 0;
+
+volatile uint8_t bumpData = 0;
+
+void delay_ms(uint32_t ms) {
+    for (uint32_t i = 0; i < ms * 3000; i++) {
+        __NOP();
+    }
 }
+
+void DebugDump(uint8_t bumpData, uint8_t lineData) {
+    bumpDataArray[debugIndex] = bumpData;
+    lineDataArray[debugIndex] = lineData;
+    debugIndex = (debugIndex + 1) % DEBUG_SIZE;
+}
+
+
 
 int main(void) {
     // write this as part of Lab 2
     
     ClockInit48MHz();
+    BumpInit();
 
     while (true) {
-        WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // Stop watchdog timer
-        // write this as part of Lab 2
-        // 10ms delay
-        // debug dump
+        bumpData = BumpRead();
+        uint8_t lineData = 0; 
+        DebugDump(bumpData, lineData);
+        delay_ms(10);
+        
     }
 }
